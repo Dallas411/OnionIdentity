1783d1827b7a46b338a50c2f9d0bdfd51afd3a65
Marco Dalla Libera <marco@dallalibera.net>
2018-03-30 15:32:54 +0200

    Aggiungere i file di progetto.

---
+++ OnionIdentity.Services/EmailSenderService.cs
@@ -0,0 +1,18 @@
+﻿using OnionIdentity.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnionIdentity.Services
+{
+    // This class is used by the application to send email for account confirmation and password reset.
+    // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
+    public class EmailSenderService : IEmailSenderService
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
